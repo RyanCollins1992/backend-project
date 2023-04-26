71c9e1d7fad4519d1ce471b26b52fe9c1e6c2093
Ryan Collins <33629095+RyanCollins1992@users.noreply.github.com>
2023-04-26 16:52:49 +0100

    Add files via upload

---
+++ src/test/java/com/ryan/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
